da964fdd2f1b36fc09afa0fce7d1d094b6e34001
genaromartinez <genaro.mtz1998@gmail.com>
2020-09-14 01:59:08 -0500

    Revert "add all functionalities"
    This reverts commit 2784f5fda0a4174772ce22a52c69d1e584c49cf2.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="https://vuejs.org/">Vue is awesome</a>
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <router-link to="/" class="nav-link">
-              Home
-            </router-link>
-            <router-link to="/projects" class="nav-link" >Projects</router-link>
-            <router-link to="/services" class="nav-link" >Services</router-link>
-        </ul>
-      </div>
-    </nav>
-      <router-view></router-view>
+    <Perritos/>
   </div>
 </template>
 
 <script>
+import Perritos from './components/Perritos';
+
 export default {
   name: 'App',
+  components: {
+    Perritos,
+  },
 };
 </script>
 
